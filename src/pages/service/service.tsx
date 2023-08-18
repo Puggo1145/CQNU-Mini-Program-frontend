584f9177e90d7cb5c14d4b9fa4d5b6e08e5ed1ba
Andrew Zhao <puggoo1145@gmail.com>
2023-08-18 19:51:54 +0800

    service ui finished

--- src/pages/service/service.tsx
+++ src/pages/service/service.tsx
@@ -1,17 +1,61 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
+import { View, Text, Image } from '@tarojs/components'
+import useStore from '@/store/store'
 
 import './service.css'
 
+import classRoomNavImg from '../../static/service/classroomNav.png'
+import mtAppointmentImg from '../../static/service/appointment.png'
+import chooseClassImg from '../../static/service/chooseClass.png'
+import getWaterImg from '../../static/service/getWater.png'
+import paymentImg from '../../static/service/payment.png'
+import runErrandsImg from '../../static/service/runErrands.png'
+
 export default function Service() {
 
-  useLoad(() => {
-    console.log('Page loaded.')
-  })
+  const statusBarHeight = useStore((state) => state.statusBarHeight)
 
   return (
-    <View className='service'>
-      <Text>Hello world!</Text>
+    <View className='service-wrapper' style={{ paddingTop: statusBarHeight + 'px' }}>
+      <View className='service-header'>服务中心</View>
+      <View className='service-content'>
+        <View className='service-content-section'>
+          <Text>学业服务</Text>
+          <View className='service-items'>
+            <View className='service-classroomNav service-item'>
+              <Image src={classRoomNavImg}></Image>
+              教室导航
+            </View>
+            <View className='service-mtAppointment service-item'>
+              <Image src={mtAppointmentImg}></Image>
+              梦厅预约
+            </View>
+            <View className='service-choooseClass service-item'>
+              <Image src={chooseClassImg}></Image>
+              快捷选课
+            </View>
+          </View>
+        </View>
+        <View className='service-content-section'>
+          <Text>生活服务</Text>
+          <View className='service-items'>
+            <View className='service-getWater service-item'>
+              <Image src={getWaterImg}></Image>
+              送水服务
+            </View>
+            <View className='service-payment service-item'>
+              <Image src={paymentImg}></Image>
+              生活缴费
+            </View>
+            <View className='service-runErrands service-item'>
+              <Image src={runErrandsImg}></Image>
+              跑腿代取
+            </View>
+          </View>
+        </View>
+        {/* <View className='service-content-section'>
+          <Text>入驻服务</Text>
+        </View> */}
+      </View>
     </View>
   )
 }
