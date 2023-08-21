965fc27794e5bc5f7ab12a07ccc37a5afefd502a
Andrew Zhao <puggoo1145@gmail.com>
2023-08-21 16:41:16 +0800

    study ui finished

--- src/pages/study/study.tsx
+++ src/pages/study/study.tsx
@@ -1,17 +1,76 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
+import { View, Text, Image } from '@tarojs/components'
+
+import useStore from '@/store/store'
 
 import './study.css'
 
+import bookCover from '../../static/reading/cover.png'
+
 export default function Study() {
 
-  useLoad(() => {
-    console.log('Page loaded.')
-  })
+  const statusBarHeight = useStore(state => state.statusBarHeight)
 
   return (
-    <View className='study'>
-      <Text>Hello world!</Text>
+    <View className='study-wrapper' style={{ paddingTop: statusBarHeight + 'px' }}>
+      <View className='study-header'>专业名称</View>
+      <View className='study-content'>
+        <View className='study-timetable'>
+          <View className='study-timetable-top'>
+            <Text>我的课程</Text>
+            <View className='study-timetable-toTable'>查看课表></View>
+          </View>
+          <View className='study-timetable-items'>
+            <View className='study-timetable-current'>
+              <View className='study-timetable-left'>
+                <Text>距离下一节课还有4分钟</Text>
+                <Text className='study-timetable-name'>产品运营</Text>
+                <Text>教室：</Text>
+              </View>
+              <View className='study-timetable-line'></View>
+              <View className='study-timetable-right'>
+                <Text>10:30</Text>
+              </View>
+            </View>
+            <View className='study-timetable-following'>
+              <View className='study-timetable-following-wrapper'>
+                <View className='study-timetable-item'>
+                  <Text>网页设计与制作</Text>
+                  <Text>14:30</Text>
+                </View>
+                <View className='study-timetable-item'>
+                  <Text>管理学基础</Text>
+                  <Text>16:30</Text>
+                </View>
+                <View className='study-timetable-item'>
+                  <Text>数字产业</Text>
+                  <Text>19:30</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+        </View>
+        <View className='study-reading'>
+          <View className='study-reading-top'>
+            <Text>我的阅读</Text>
+            <View className='study-reading-toBook'>查看书架></View>
+          </View>
+          <View className='study-reading-bookShelf'>
+            <View className='study-reading-books-xsbd'>
+              <Text className='study-reading-books-title'>新生必读</Text>
+              <View className='study-reading-books'>
+                <View className='study-reading-book'>
+                  <Image src={bookCover} className='study0-reading-book-cover' mode='widthFix'></Image>
+                  <View className='study-reading-book-right'>
+                    <Text className='study-reading-book-title'>学生手册</Text>
+                    <Text className='study-reading-book-description'>学生手册是一种学生手册</Text>
+                    <View className='study-reading-book-mark'>强烈推荐</View>
+                  </View>
+                </View>
+              </View>
+            </View>
+          </View>
+        </View>
+      </View>
     </View>
   )
 }
