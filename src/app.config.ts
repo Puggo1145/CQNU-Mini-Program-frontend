7bfe24ddc7d692d11d20ff3eee5e55242c19c599
Andrew Zhao <puggoo1145@gmail.com>
2023-10-24 22:02:55 +0800

    将首页名称改为今天

--- src/app.config.ts
+++ src/app.config.ts
@@ -34,7 +34,7 @@ export default defineAppConfig({
     backgroundColor: "#fff",
     borderStyle: "white",
     list: [
-      {pagePath: 'pages/index/index', text: '首页', iconPath: './static/tabBar/home-icon.png', selectedIconPath: './static/tabBar/home-selectedIcon.png'},
+      {pagePath: 'pages/index/index', text: '今天', iconPath: './static/tabBar/home-icon.png', selectedIconPath: './static/tabBar/home-selectedIcon.png'},
       {pagePath: 'pages/study/study', text: '学习', iconPath: './static/tabBar/study-icon.png', selectedIconPath: './static/tabBar/study-selectedIcon.png'},
       {pagePath: 'pages/service/service', text: '服务', iconPath: './static/tabBar/service-icon.png', selectedIconPath: './static/tabBar/service-selectedIcon.png'},
       {pagePath: 'pages/mine/mine', text: '我的', iconPath: './static/tabBar/mine-icon.png', selectedIconPath: './static/tabBar/mine-selectedIcon.png'}
